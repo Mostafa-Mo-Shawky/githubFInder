9dc68f2724132a0c425b046718c03f34af6e4686
Mostafa-Mo-Shawky <107374651+Mostafa-Mo-Shawky@users.noreply.github.com>
2022-07-15 10:43:25 +0200

    Update GithubActions.js

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -15,7 +15,7 @@ export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
   });
-  const response = await github.get(`/search/users?${params}`);
+  const response = await github.get(`search/users?${params}`);
   return response.data.items;
 };
 
@@ -33,8 +33,8 @@ export const searchUsers = async (text) => {
 // Get user and repos
 export const getUserAndRepo = async (login) => {
   const [user, repos] = await Promise.all([
-    github.get(`/users/${login}`),
-    github.get(`/users/${login}/repos`),
+    github.get(`users/${login}`),
+    github.get(`users/${login}/repos`),
   ]);
   return { user: user.data, repos: repos.data };
 };
